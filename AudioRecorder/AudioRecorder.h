42744c3eaf587aa4504b7ac2d387d4c5c94105b9
Hugo <54809176+HUGOW04@users.noreply.github.com>
2022-07-16 14:10:31 +0200

    Add project files.

---
+++ AudioRecorder/AudioRecorder.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class AudioRecorder
+{
+public:
+	AudioRecorder();
+	~AudioRecorder();
+	void Update();
+
+	bool Init(void(*callback)(WAVEHDR data));
+
+	bool StartRecording();
+	void StopRecording();
+
+	WAVEHDR getNewWAVEHDR();
+
+private:
+	void(*callback)(WAVEHDR data);
+	bool running;
+
+	MMRESULT res;
+	HWAVEIN micHandle;
+	WAVEFORMATEX format;
+	WAVEHDR buffers[];
+
+	bool addBuffer(WAVEHDR* buffer);
+};
+
+static void CALLBACK waveInCallback(HWAVEIN hWaveOut, UINT uMesg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
+void playData(WAVEHDR hdr);
---
+++ AudioRecorder/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <Windows.h>
+#include <MMSystem.h>
+
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "ws2_32")
+
+
+int main()
+{
+	const int NUMPTS = 44100 * 10;
+	int sampleRate = 44100;
+	short int waveIn[NUMPTS];
+
+	HWAVEIN hWaveIn;
+	WAVEHDR WaveInHdr;
+	MMRESULT result;
+	HWAVEOUT hWaveOut;
+
+	WAVEFORMATEX pFormat;
+	pFormat.wFormatTag = WAVE_FORMAT_PCM;
+	pFormat.nChannels = 2;
+	pFormat.nSamplesPerSec = sampleRate;
+	pFormat.nAvgBytesPerSec = 4 * sampleRate;
+	pFormat.nBlockAlign = 4;
+	pFormat.wBitsPerSample = 16;
+	pFormat.cbSize = 0;
+
+	result = waveInOpen(&hWaveIn, WAVE_MAPPER, &pFormat, 0, 0, WAVE_FORMAT_DIRECT);
+
+	if (result)
+	{
+		char fault[256];
+		waveInGetErrorTextA(result, fault, 256);
+		MessageBoxA(NULL, fault, "Failed to open waveform input device.", MB_OK | MB_ICONEXCLAMATION);
+		return 1;
+	}
+
+	WaveInHdr.lpData = (LPSTR)waveIn;
+	WaveInHdr.dwBufferLength = 2 * NUMPTS;
+	WaveInHdr.dwBytesRecorded = 0;
+	WaveInHdr.dwUser = 0;
+	WaveInHdr.dwFlags = 0;
+	WaveInHdr.dwLoops = 0;
+	waveInPrepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+
+	result = waveInAddBuffer(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+	if (result)
+	{
+		MessageBoxA(NULL, "Failed to read block from device", NULL, MB_OK | MB_ICONEXCLAMATION);
+		return 1;
+	}
+
+	result = waveInStart(hWaveIn);
+	if (result)
+	{
+		MessageBoxA(NULL, "Failed to start recording", NULL, MB_OK | MB_ICONEXCLAMATION);
+		return 1;
+	}
+
+	std::cout << "Recording..." << std::endl;
+	Sleep((NUMPTS / sampleRate) * 1000); 
+
+	std::cout << "Playing..." << std::endl;
+
+	if (waveOutOpen(&hWaveOut, WAVE_MAPPER, &pFormat, 0, 0, WAVE_FORMAT_DIRECT))
+	{
+		MessageBoxA(NULL, "Failed to replay", NULL, MB_OK | MB_ICONEXCLAMATION);
+	}
+
+	waveOutWrite(hWaveOut, &WaveInHdr, sizeof(WaveInHdr)); 
+	Sleep((NUMPTS / sampleRate) * 1000); 
+
+	waveOutUnprepareHeader(hWaveOut, &WaveInHdr, sizeof(WAVEHDR));
+	waveInUnprepareHeader(hWaveIn, &WaveInHdr, sizeof(WAVEHDR));
+	waveInClose(hWaveIn);
+	waveOutClose(hWaveOut);
+
+	return 0;
+}
